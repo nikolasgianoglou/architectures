2a67e3d2486fba643aef99bed46471479a4af1b6
nikolascoelho <nikolasgianoglou.coelho-ext@ab-inbev.com>
2023-09-18 15:17:01 -0300

    Creation of the initial project

---
+++ Arquiteturas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Arquiteturas
+//
+//  Created by Nikolas Gianoglou Coelho on 18/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
